2849702fd6b2ded21504df57570731252575bf4c
CCSensus <122903758+CCSensus@users.noreply.github.com>
2023-01-17 17:44:23 +0100

    commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -18,5 +18,83 @@ public class DemoApplication {
 	String sayHello() {
     return "Hello Azure!";
 }
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Übung 1.</title>
+    </head>
+    <body>
+<H1> Berechnung der Fallgeschwindigkeit </h1>
+        <?php echo"<b> Hallo </b>, diese Webseite dient zur Berechnung der Fallgeschwindigkeit!";?>
+<br></br>
+
+<form>
+  Gib deine Starthoehe ein: <input name=Starthoehe size=7>
+</form>
+
+<?php
+
+$Starthoehe=$_GET ['Starthoehe'];
+if (is_numeric($Starthoehe)) echo "<br> Ihre Starthoehe beträgt <b> $Starthoehe Meter! </b> <br>";
+else echo "<br> Ihr Angabe ist keine Zahl";
+$g = 9.81;
+$s = 2;
+$counter = 0;
+$Ergebnis= $g * $s;
+$fallgeschwindigkeit = sqrt($Ergebnis*$Starthoehe) ;
+$aufgerundet = ceil ($fallgeschwindigkeit);
+$Rfallgeschwindigkeit= sqrt($Ergebnis*$counter) ;
+$Raufgerundet = ceil ($Rfallgeschwindigkeit);
+
+
+
+echo "<table border=1>";
+echo "<br><br>Ihre Aufprallgeschwindigkeit von $Starthoehe Meter betraegt: $aufgerundet m/s" ;
+
+
+
+
+while($Starthoehe>=0) {
+$Rfallgeschwindigkeit= sqrt($Ergebnis*$counter) ;
+$Raufgerundet = ceil ($Rfallgeschwindigkeit);
+
+if($Starthoehe<1000) {
+echo "<tr> <th>$Starthoehe m Fallhoehe </th>";
+echo " <th> $Raufgerundet M/s </th>"; 
+
+
+$counter=$counter+100;
+$Starthoehe= $Starthoehe-100;
+
+
+}
+
+else {
+
+$Rfallgeschwindigkeit= sqrt($Ergebnis*$counter) ;
+$Raufgerundet = ceil ($Rfallgeschwindigkeit);
+
+
+
+echo "<tr> <th>$Starthoehe m Fallhoehe </th>";
+echo " <th> $Raufgerundet M/s </th>"; 
+
+
+$counter=$counter+500;
+$Starthoehe= $Starthoehe-500;
+
+
+}
+
+}
+
+
+
+?>
+
+    </body>
+</html>
+
 	}
 
